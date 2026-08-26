a3c4f0c5313c2032b10880daeb085c93bc712d57
MayankShukla-dev <mayankshukla06364@gmail.com>
2026-08-26 22:27:06 +0530

    blogpage

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+     <h1 className= 'text-yellow-500'> A blog page</h1>
+    </>
+  )
+}
+
+export default App
