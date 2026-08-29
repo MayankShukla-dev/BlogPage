e40c76af92d90aedf544eafb48876a6697a51e3f
MayankShukla-dev <mayankshukla06364@gmail.com>
2026-08-29 21:22:02 +0530

    update mega blog page

---
+++ src/components/Select.jsx
@@ -0,0 +1,26 @@
+import React, {useId} from 'react'
+
+function Select({
+    options,
+    label,
+    className ,
+    ...props
+
+},ref) {
+    const id = useId()
+  return (
+    <div className='w-full'>
+      {label && <label htmlFor={id} className=''></label>}
+      <select
+      {...porps}
+      id={id}
+      
+      >
+
+      </select>
+
+    </div>
+  )
+}
+
+export default Select
