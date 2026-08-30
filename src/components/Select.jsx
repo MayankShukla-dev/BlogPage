d4f7254547853e35ff2588e616632a052d0d4b95
MayankShukla-dev <mayankshukla06364@gmail.com>
2026-08-30 22:13:47 +0530

    Mega Blog Page

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -12,10 +12,16 @@ function Select({
     <div className='w-full'>
       {label && <label htmlFor={id} className=''></label>}
       <select
-      {...porps}
+      {...props}
       id={id}
-      
+      ref={ref}
+      className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
       >
+        {options?.map((option) => {
+          <option key ={option} value={option}>
+            {option}
+          </option>
+        })}
 
       </select>
 
@@ -23,4 +29,4 @@ function Select({
   )
 }
 
-export default Select
+export default React.forwardRef(Select)
